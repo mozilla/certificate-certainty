be65913fc2875371537be37dad6c566003e471d5
Hal Wine <hwine@mozilla.com>
2022-05-25 16:09:29 -0700

    Make more testable by humans
    - shorten backoff interval on retries
    - allow sigint to stop scan, but output current state

--- custom_requests.py
+++ custom_requests.py
@@ -3,6 +3,7 @@
 
 import requests
 from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 ############################################################
 #
@@ -40,14 +41,12 @@ http = requests.Session()
 #
 ############################################################
 
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 
 retry_strategy = Retry(
     total=10,
     status_forcelist=[429, 500, 502, 503, 504],
     method_whitelist=["HEAD", "GET", "OPTIONS"],
-    backoff_factor=10,
+    backoff_factor=0.1,
 )
 # adapter = HTTPAdapter(max_retries=retry_strategy)
 http = requests.Session()
@@ -55,4 +54,4 @@ http.mount("https://", TimeoutHTTPAdapter(max_retries=retry_strategy))
 http.mount("http://", TimeoutHTTPAdapter(max_retries=retry_strategy))
 
 
-http.mount("https://", TimeoutHTTPAdapter(max_retries=retry_strategy))
+# http.mount("https://", TimeoutHTTPAdapter(max_retries=retry_strategy))
